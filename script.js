780e80733f4827db8efece79dd9f1d2a9d0ea6f3
i-Riyaj <riyajriyaj452@gmail.com>
2023-12-05 13:00:50 +0530

    6th commit

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-const words = ["rock", "paper", "scissor"];
-    let computerInp = words[Math.floor(
-    Math.random()*(
-        (words.length))
-    )];
-let userInp = prompt(`choose any word from:  ${words} :`);
-if(
-    (computerInp == "rock" && userInp == "scissor")||
-     (computerInp == "paper" && userInp == "rock")||
-     (computerInp == "seissor" && userInp == "paper")
-){
-    let p1 = document.createElement("p");
-    p1.innerHTML = ("Alas! You Lost the match.");
-    document.body.querySelector("div").appendChild(p1);
+const rpsGame = () =>{
+    const words = ["rock", "paper", "scissor"];
+
+    const compInp = words[Math.floor((Math.random()*(words.length)))];
+    
+    
+    const userInput = document.querySelector("#userInput");
+    const btn1 =document.querySelector("#btn1");
+
+    const comp_Gen_Word = document.querySelector("#compGenWord");
+    const user_Entered_Word = document.querySelector("#userEnteredWord");
+    
+    const userInp = userInput.value;
+    const result = document.querySelector("#resultText");
+
+    if(
+        (compInp == "rock" && userInp == "scissor")||
+        (compInp == "paper" && userInp == "rock")||
+        (compInp == "seissor" && userInp == "paper")
+    ){
+        result.innerHTML = "Sorry!You Lost the round.";
+        comp_Gen_Word.innerHTML = `Random generated word was : ${compInp}.`;
+        user_Entered_Word.innerHTML =`Your Entered: ${userInp}.`;
+        }
+        else if(
+        (compInp == "scissor" && userInp == "rock")||
+        (compInp == "rock" && userInp == "paper")||
+        (compInp == "paper" && userInp == "scissor")
+    ){
+        result.innerHTML = "Hurrah!You won the round.";
+        comp_Gen_Word.innerHTML = `Random generated word was : ${compInp}.`;
+        user_Entered_Word.innerHTML =`Your Entered: ${userInp}.`;
+    }
+    else if(
+        (compInp == "rock" && userInp == "rock")||
+        (compInp == "paper" && userInp == "paper")||
+        (compInp == "scissor" && userInp == "scissor")
+    ){
+        result.innerHTML = "It's a tie.";
+        comp_Gen_Word.innerHTML = `Random generated word was : ${compInp}.`;
+        user_Entered_Word.innerHTML =`Your Entered: ${userInp}.`;
+    }
+    else{
+        result.innerHTML = "Something Wrong.";
+    }
+    const output = document.querySelector(".outputSection");
+    output.className = "resultArea";     
 }
-else if(
-    (computerInp == "scissor" && userInp == "rock")||
-     (computerInp == "rock" && userInp == "paper")||
-     (computerInp == "paper" && userInp == "scissor")
-){
-    let p1 = document.createElement("p");
-    p1.innerHTML = ("Hurrah! You won the match.");
-    document.body.querySelector("div").appendChild(p1);
-}
-else if(
-    (computerInp == "rock" && userInp == "rock")||
-     (computerInp == "paper" && userInp == "paper")||
-     (computerInp == "scissor" && userInp == "scissor")
-){
-    let p1 = document.createElement("p");
-    p1.innerHTML = ("It's a tie.");
-    document.body.querySelector("div").appendChild(p1);
-}
-else{
-    let p1 = document.createElement("p");
-    p1.innerHTML = ("something gone wrong.");
-    document.body.querySelector("div").appendChild(p1);
-}
-let p2 = document.createElement("p");
-    p2.innerHTML = (`You entered: ${userInp};  \ncomputer generated word: ${computerInp}`);
-    document.body.querySelector("div").appendChild(p2);
-let parentDiv = document.querySelector("div");
-parentDiv.className = "flexDiv";
+btn1.addEventListener('click',rpsGame); 
+
+
